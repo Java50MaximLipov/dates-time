3ba30eb1dcc2da72e0b523112750b0408e9e7cc4
mlipov <maxim.lipov@gmail.com>
2023-08-17 19:50:16 +0300

    HW-24 NextFriday13

--- src/telran/time/NextFriday13.java
+++ src/telran/time/NextFriday13.java
@@ -1,14 +1,35 @@
 package telran.time;
 
+import java.time.temporal.ChronoField;
+import java.time.temporal.ChronoUnit;
 import java.time.temporal.Temporal;
 import java.time.temporal.TemporalAdjuster;
+import java.time.temporal.UnsupportedTemporalTypeException;
 
 public class NextFriday13 implements TemporalAdjuster {
 
 	@Override
 	public Temporal adjustInto(Temporal temporal) {
-		// TODO Auto-generated method stub
-		return null;
+		if (!temporal.isSupported(ChronoUnit.YEARS)) {
+			throw new UnsupportedTemporalTypeException("must support years");
+		}
+		if (temporal.get(ChronoField.DAY_OF_MONTH) == 13) {
+			return temporal = monthPlus(temporal);
+		}
+		do {
+			temporal = temporal.plus(1, ChronoUnit.DAYS);
+		} while (temporal.get(ChronoField.DAY_OF_MONTH) != 13);
+		if (temporal.get(ChronoField.DAY_OF_WEEK) != 5) {
+			temporal = monthPlus(temporal);
+		}
+		return temporal;
+	}
+
+	private Temporal monthPlus(Temporal temporal) {
+		do {
+			temporal = temporal.plus(1, ChronoUnit.MONTHS);
+		} while (temporal.get(ChronoField.DAY_OF_WEEK) != 5);
+		return temporal;
 	}
 
 }
